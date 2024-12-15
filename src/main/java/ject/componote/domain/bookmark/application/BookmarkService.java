8988364c37efb4ac6dc20c8fcd698097817397aa
SSUHYUNKIM <yellosuhyun@naver.com>
2024-12-16 00:15:27 +0900

    [FEAT] BookmarkRepository 작업 (#02)

--- src/main/java/ject/componote/domain/bookmark/application/BookmarkService.java
+++ src/main/java/ject/componote/domain/bookmark/application/BookmarkService.java
@@ -4,6 +4,7 @@ import ject.componote.domain.auth.domain.Member;
 import ject.componote.domain.auth.domain.MemberRepository;
 import ject.componote.domain.auth.model.AuthPrincipal;
 import ject.componote.domain.bookmark.domain.Bookmark;
+import ject.componote.domain.bookmark.domain.BookmarkRepository;
 import ject.componote.domain.component.domain.Component;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,7 @@ public class BookmarkService {
     private final MemberRepository memberRepository;
 
     public BookmarkResponse addBookmark(AuthPrincipal authPrincipal, Long componentId) {
-        if (bookmarkRepository.existsByIdAndComponentId(authPrincipal.id(), componentId)) {
+        if (bookmarkRepository.existsByUserIdAndComponentId(authPrincipal.id(), componentId)) {
             throw new IllegalArgumentException("이미 북마크에 추가된 컴포넌트입니다.");
         }
 
---
+++ src/main/java/ject/componote/domain/bookmark/domain/BookmarkRepository.java
@@ -0,0 +1,14 @@
+package ject.componote.domain.bookmark.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    boolean existsByUserIdAndComponentId(Long userId, Long componentId);
+
+    List<Bookmark> findAllByUserId(Long userId);
+
+    Optional<Bookmark> findByUserIdAndComponentId(Long userId, Long componentId);
+}
