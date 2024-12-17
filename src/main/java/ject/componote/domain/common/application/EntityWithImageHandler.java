bc7dc2e987dc99251a9da9ec9e60f5aada1f1c86
kmw2378 <rlarla677@gmail.com>
2024-12-17 14:02:21 +0900

    [FEAT] EntityWithImageHandler 추가 이미지를 포함하는 엔티티 저장시 사용되는 클래스 추가

---
+++ src/main/java/ject/componote/domain/common/application/EntityWithImageHandler.java
@@ -0,0 +1,26 @@
+package ject.componote.domain.common.application;
+
+import ject.componote.global.util.ObjectKeyTransformer;
+import ject.componote.infra.file.application.FileClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.function.Function;
+
+@Component
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class EntityWithImageHandler {
+    private final FileClient fileClient;
+
+    @Transactional
+    public <T> T persist(final String tempKey, final Function<String, T> callback) {
+        final String permanentKey = ObjectKeyTransformer.toPermanentKey(tempKey);
+        fileClient.moveFile(tempKey, permanentKey)
+                .publishOn(Schedulers.boundedElastic())
+                .block();
+        return callback.apply(permanentKey);
+    }
+}
