219d6098cfc96ce55eb862132aca8145b4223c3f
kmw2378 <rlarla677@gmail.com>
2024-12-17 13:48:52 +0900

    [FEAT] InvalidJobException 추가

---
+++ src/main/java/ject/componote/domain/auth/error/InvalidJobException.java
@@ -0,0 +1,10 @@
+package ject.componote.domain.auth.error;
+
+import org.springframework.http.HttpStatus;
+
+public class InvalidJobException extends AuthException {
+
+    public InvalidJobException(final String value) {
+        super("직종이 올바르지 않습니다. 입력한 직종: " + value, HttpStatus.BAD_REQUEST);
+    }
+}
