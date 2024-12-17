c2ebb8f02ab3dd88c248dfb34a864c754c216633
kmw2378 <rlarla677@gmail.com>
2024-12-17 14:08:21 +0900

    [FEAT] SocialAccountRepository.findBySocialIdAndProviderType() 메서드 추가

--- src/main/java/ject/componote/domain/auth/domain/SocialAccountRepository.java
+++ src/main/java/ject/componote/domain/auth/domain/SocialAccountRepository.java
@@ -2,5 +2,8 @@ package ject.componote.domain.auth.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SocialAccountRepository extends JpaRepository<SocialAccount, Long> {
+    Optional<SocialAccount> findBySocialIdAndProviderType(final String socialId, final ProviderType providerType);
 }
