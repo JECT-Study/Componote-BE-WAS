a880a85d96319a00f9996448d4832a5bf86c8d9b
kmw2378 <rlarla677@gmail.com>
2024-12-17 14:02:38 +0900

    [FEAT] FileClient 추가 파일 서버 API 호출 클래스 추가

---
+++ src/main/java/ject/componote/infra/file/application/FileClient.java
@@ -0,0 +1,69 @@
+package ject.componote.infra.file.application;
+
+import ject.componote.global.error.ErrorResponse;
+import ject.componote.infra.file.dto.move.request.MoveRequest;
+import ject.componote.infra.file.error.FileClientException;
+import ject.componote.infra.util.TimeoutDecorator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+@Slf4j
+public class FileClient {
+    private final int maxRetry;
+    private final int timeout;
+    private final TimeoutDecorator timeoutDecorator;
+    private final WebClient webClient;
+
+    public FileClient(@Value("${file.max-retry}") final int maxRetry,
+                      @Value("${file.timeout}") final int timeout,
+                      final TimeoutDecorator timeoutDecorator,
+                      final WebClient webClient) {
+        this.maxRetry = maxRetry;
+        this.timeout = timeout;
+        this.timeoutDecorator = timeoutDecorator;
+        this.webClient = webClient;
+    }
+
+    public Mono<Void> moveFile(final String tempKey, final String permanentKey) {
+        return timeoutDecorator.decorate(move(tempKey, permanentKey), maxRetry, timeout);
+    }
+
+    private Mono<Void> move(final String tempKey, final String permanentKey) {
+        return webClient.post()
+                .uri("http://localhost:8081/move")
+                .bodyValue(MoveRequest.of(tempKey, permanentKey))
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, this::handle4xxError)
+                .onStatus(HttpStatusCode::is5xxServerError, this::handle5xxError)
+                .toBodilessEntity()
+                .then()
+                .doOnSuccess(ignore -> handleSuccess(permanentKey))
+                .doOnError(error -> handleError(tempKey, permanentKey, error));
+    }
+
+    private Mono<? extends Throwable> handle5xxError(final ClientResponse clientResponse) {
+        return clientResponse.bodyToMono(ErrorResponse.class)
+                .map(ErrorResponse::getMessage)
+                .map(IllegalStateException::new);
+    }
+
+    private Mono<? extends Throwable> handle4xxError(final ClientResponse clientResponse) {
+        return clientResponse.bodyToMono(ErrorResponse.class)
+                .map(FileClientException::new);
+    }
+
+    private void handleSuccess(final String permanentKey) {
+        log.info("File moved successfully: {}", permanentKey);
+    }
+
+
+    private void handleError(final String tempKey, final String permanentKey, final Throwable error) {
+        log.error("Failed to move file from {} to {}: {}", tempKey, permanentKey, error.getMessage());
+    }
+}
