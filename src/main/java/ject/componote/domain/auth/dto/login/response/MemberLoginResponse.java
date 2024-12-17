dcbd3950747bb7a7f53c633ca9aafd1135976a82
kmw2378 <rlarla677@gmail.com>
2024-12-17 14:01:24 +0900

    [FEAT] MemberLoginResponse 추가

--- src/main/java/ject/componote/domain/auth/dto/login/response/MemberLoginResponse.java
+++ src/main/java/ject/componote/domain/auth/dto/login/response/MemberLoginResponse.java
@@ -1,4 +1,9 @@
 package ject.componote.domain.auth.dto.login.response;
 
+import ject.componote.domain.auth.domain.Member;
+
 public record MemberLoginResponse(String accessToken, Long memberId) {
+    public static MemberLoginResponse of(final String accessToken, final Member member) {
+        return new MemberLoginResponse(accessToken, member.getId());
+    }
 }
