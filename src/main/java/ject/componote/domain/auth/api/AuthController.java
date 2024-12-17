3de2b66e3ad1620a82267654356c9649867b32aa
kmw2378 <rlarla677@gmail.com>
2024-12-17 14:08:59 +0900

    [FEAT] AuthController 추가

--- src/main/java/ject/componote/domain/auth/api/AuthController.java
+++ src/main/java/ject/componote/domain/auth/api/AuthController.java
@@ -2,34 +2,33 @@ package ject.componote.domain.auth.api;
 
 import jakarta.validation.Valid;
 import ject.componote.domain.auth.application.AuthService;
-import ject.componote.domain.auth.dto.authorize.request.MemberAuthorizeRequest;
-import ject.componote.domain.auth.dto.authorize.response.MemberAuthorizeResponse;
 import ject.componote.domain.auth.dto.login.request.MemberLoginRequest;
 import ject.componote.domain.auth.dto.login.response.MemberLoginResponse;
+import ject.componote.domain.auth.dto.signup.request.MemberSignupRequest;
+import ject.componote.domain.auth.dto.signup.response.MemberSignupResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/oauth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
     private final AuthService authService;
 
-    @GetMapping("/authorize")
-    public ResponseEntity<MemberAuthorizeResponse> authorize(@ModelAttribute @Valid final MemberAuthorizeRequest memberAuthorizeRequest) {
-        return ResponseEntity.ok()
-                .body(authService.authorize(memberAuthorizeRequest));
+    @PostMapping("/signup")
+    public ResponseEntity<MemberSignupResponse> signup(@RequestBody @Valid final MemberSignupRequest memberSignupRequest) {
+        return ResponseEntity.ok(
+                authService.signup(memberSignupRequest));
     }
 
     @PostMapping("/login")
     public ResponseEntity<MemberLoginResponse> login(@RequestBody @Valid final MemberLoginRequest memberLoginRequest) {
-        return ResponseEntity.ok()
-                .body(authService.login(memberLoginRequest));
+        return ResponseEntity.ok(
+                authService.login(memberLoginRequest)
+        );
     }
 }
