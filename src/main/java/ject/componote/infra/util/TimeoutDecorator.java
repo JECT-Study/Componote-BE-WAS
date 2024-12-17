e7fed7320cff8235144d6ed5e23334f8757cf83b
kmw2378 <rlarla677@gmail.com>
2024-12-17 13:56:46 +0900

    [REFACTOR] OAuth 관련 timeout, retry 설정 수정

--- src/main/java/ject/componote/infra/util/TimeoutDecorator.java
+++ src/main/java/ject/componote/infra/util/TimeoutDecorator.java
@@ -1,6 +1,5 @@
-package ject.componote.infra.oauth.application.decorator;
+package ject.componote.infra.util;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
@@ -9,23 +8,14 @@ import java.time.Duration;
 import java.util.concurrent.TimeoutException;
 
 @Component
-public class OAuthTimeoutDecorator {
-    private final int timeout;
-    private final int maxRetry;
-
-    public OAuthTimeoutDecorator(@Value("${oauth.timeout}") final int timeout,
-                                 @Value("${oauth.max-retry}") final int maxRetry) {
-        this.timeout = timeout;
-        this.maxRetry = maxRetry;
-    }
-
-    public <T> Mono<T> decorate(final Mono<T> mono) {
+public class TimeoutDecorator {
+    public <T> Mono<T> decorate(final Mono<T> mono, final int maxRetry, final int timeout) {
         return mono.timeout(Duration.ofMillis(timeout))
                 .retryWhen(Retry.max(maxRetry).filter(this::isRetryable))
                 .onErrorMap(TimeoutException.class, e -> new IllegalStateException("요청 시간이 초과되었습니다.", e));
     }
 
-    private boolean isRetryable(Throwable ex) {
+    private boolean isRetryable(final Throwable ex) {
         return (ex instanceof IllegalStateException) || (ex instanceof TimeoutException);
     }
 }
