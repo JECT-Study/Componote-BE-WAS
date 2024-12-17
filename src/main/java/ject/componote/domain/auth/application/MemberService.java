6813f819013fdd42e46960f8093de0d252a75329
kmw2378 <rlarla677@gmail.com>
2024-12-17 14:08:46 +0900

    [REFACTOR] MemberService.getSummary() 메서드 내부 수정

--- src/main/java/ject/componote/domain/auth/application/MemberService.java
+++ src/main/java/ject/componote/domain/auth/application/MemberService.java
@@ -1,16 +1,23 @@
 package ject.componote.domain.auth.application;
 
+import ject.componote.domain.auth.domain.MemberRepository;
 import ject.componote.domain.auth.dto.find.response.MemberSummaryResponse;
+import ject.componote.domain.auth.error.NotFoundMemberException;
 import ject.componote.domain.auth.model.AuthPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
-    private final MemberFindService memberFindService;
+    private final MemberRepository memberRepository;
 
     public MemberSummaryResponse getSummary(final AuthPrincipal authPrincipal) {
-        return memberFindService.findSummaryByMemberId(authPrincipal.id());
+        final Long memberId = authPrincipal.id();
+        return memberRepository.findSummaryById(memberId)
+                .map(MemberSummaryResponse::from)
+                .orElseThrow(() -> NotFoundMemberException.createWhenInvalidMemberId(memberId));
     }
 }
