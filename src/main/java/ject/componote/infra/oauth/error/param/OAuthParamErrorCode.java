5d6518428cdf8548a247324920d843d1e368cd42
kmw2378 <rlarla677@gmail.com>
2024-12-17 14:07:02 +0900

    [REFACTOR] OAuthParamErrorCode 수정

--- src/main/java/ject/componote/infra/oauth/error/param/OAuthParamErrorCode.java
+++ src/main/java/ject/componote/infra/oauth/error/param/OAuthParamErrorCode.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum OAuthParamErrorCode {
     INVALID_AUTHORIZATION_CODE("인가 코드는 널이거나 공백일 수 없습니다"),
-    INVALID_PROVIDER_NAME("소셜 이름을 널이거나 공백일 수 없습니다.");
+    INVALID_PROVIDER_TYPE("소셜 이름을 널이거나 공백일 수 없습니다.");
 
     private final String message;
 
