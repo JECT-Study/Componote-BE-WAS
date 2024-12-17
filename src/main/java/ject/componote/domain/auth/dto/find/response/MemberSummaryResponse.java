71078ba0de2569bd6090481f306c6f5fe0e8014a
kmw2378 <rlarla677@gmail.com>
2024-12-17 22:12:37 +0900

    [REFACTOR] 간단 회원 조회 응답 필드명 수정

--- src/main/java/ject/componote/domain/auth/dto/find/response/MemberSummaryResponse.java
+++ src/main/java/ject/componote/domain/auth/dto/find/response/MemberSummaryResponse.java
@@ -2,11 +2,11 @@ package ject.componote.domain.auth.dto.find.response;
 
 import ject.componote.domain.auth.dto.find.MemberSummaryDto;
 
-public record MemberSummaryResponse(String nickname, String profilePhotoUrl) {
+public record MemberSummaryResponse(String nickname, String profileImageUrl) {
     public static MemberSummaryResponse from(MemberSummaryDto memberSummaryDto) {
         return new MemberSummaryResponse(
                 memberSummaryDto.nickname().getValue(),
-                memberSummaryDto.profilePhoto().getObjectKey()
+                memberSummaryDto.profileImage().getObjectKey()
         );
     }
 }
