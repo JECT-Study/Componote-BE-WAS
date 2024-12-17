df0deeeaab9d73a76bc2537012f754a4c0805dc0
kmw2378 <rlarla677@gmail.com>
2024-12-17 18:18:10 +0900

    [REFACTOR] Member of() 메서드 수정 반영

--- src/main/java/ject/componote/domain/auth/dto/signup/request/MemberSignupRequest.java
+++ src/main/java/ject/componote/domain/auth/dto/signup/request/MemberSignupRequest.java
@@ -1,18 +1,12 @@
 package ject.componote.domain.auth.dto.signup.request;
 
 import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import ject.componote.domain.auth.domain.Member;
 
 public record MemberSignupRequest(
-        @Email String email,
         @NotBlank String nickname,
         @NotBlank String job,
         @Nullable String profileImageTempKey,
         @NotNull Long socialAccountId) {
-    public Member toMember(final Long socialAccountId, final String permanentKey) {
-        return Member.of(nickname, email, job, permanentKey, socialAccountId);
-    }
 }
--- src/test/java/ject/componote/fixture/MemberFixture.java
+++ src/test/java/ject/componote/fixture/MemberFixture.java
@@ -1,23 +1,22 @@
 package ject.componote.fixture;
 
 import ject.componote.domain.auth.domain.Member;
+import ject.componote.domain.common.model.Image;
 
 public enum MemberFixture {
-    KIM("김민우", "rlarla677@gmail.com", "DEVELOPER", "permanent/123.jpg");
+    KIM("김민우", "DEVELOPER", "permanent/123.jpg");
 
     private final String nickname;
-    private final String email;
     private final String job;
     private final String profileImage;
 
-    MemberFixture(final String nickname, final String email, final String job, final String profileImage) {
+    MemberFixture(final String nickname, final String job, final String profileImage) {
         this.nickname = nickname;
-        this.email = email;
         this.job = job;
         this.profileImage = profileImage;
     }
 
     public Member 생성(final Long socialAccountId) {
-        return Member.of(nickname, email, job, profileImage, socialAccountId);
+        return Member.of(nickname, job, Image.from(profileImage), socialAccountId);
     }
 }
