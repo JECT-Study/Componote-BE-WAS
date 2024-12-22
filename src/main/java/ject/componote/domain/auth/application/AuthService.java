9cfa33d791c85ac844a846db9946fb636ea28775
kmw2378 <rlarla677@gmail.com>
2024-12-22 23:51:14 +0900

    [REFACTOR] AuthService에 EntityWithImageHandler 의존성 제거 후 FileService 의존성 추가

--- src/main/java/ject/componote/domain/auth/application/AuthService.java
+++ src/main/java/ject/componote/domain/auth/application/AuthService.java
@@ -1,30 +1,32 @@
 package ject.componote.domain.auth.application;
 
+import ject.componote.domain.auth.dao.MemberRepository;
+import ject.componote.domain.auth.dao.SocialAccountRepository;
 import ject.componote.domain.auth.domain.Member;
-import ject.componote.domain.auth.domain.MemberRepository;
-import ject.componote.domain.auth.domain.SocialAccountRepository;
 import ject.componote.domain.auth.dto.login.request.MemberLoginRequest;
 import ject.componote.domain.auth.dto.login.response.MemberLoginResponse;
 import ject.componote.domain.auth.dto.signup.request.MemberSignupRequest;
 import ject.componote.domain.auth.dto.signup.response.MemberSignupResponse;
+import ject.componote.domain.auth.dto.validate.request.MemberEmailValidateRequest;
+import ject.componote.domain.auth.dto.validate.request.MemberNicknameValidateRequest;
+import ject.componote.domain.auth.error.DuplicatedNicknameException;
 import ject.componote.domain.auth.error.DuplicatedSignupException;
 import ject.componote.domain.auth.error.NotFoundMemberException;
 import ject.componote.domain.auth.error.NotFoundSocialAccountException;
 import ject.componote.domain.auth.model.AuthPrincipal;
-import ject.componote.domain.auth.util.MemberMapper;
+import ject.componote.domain.auth.model.Nickname;
 import ject.componote.domain.auth.util.TokenProvider;
-import ject.componote.domain.common.application.EntityWithImageHandler;
+import ject.componote.infra.file.application.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class AuthService {
+    private final FileService fileService;
     private final MemberRepository memberRepository;
     private final SocialAccountRepository socialAccountRepository;
     private final TokenProvider tokenProvider;
-    private final EntityWithImageHandler entityWithImageHandler;
-    private final MemberMapper memberMapper;
 
     public MemberSignupResponse signup(final MemberSignupRequest request) {
         final Long socialAccountId = request.socialAccountId();
@@ -36,11 +38,8 @@ public class AuthService {
             throw new DuplicatedSignupException(socialAccountId);
         }
 
-        final Member member = entityWithImageHandler.persist(
-                request.profileImageTempKey(),
-                permanentKey -> memberRepository.save(memberMapper.mapFrom(request, permanentKey))
-        );
-
+        final Member member = memberRepository.save(request.toMember());
+        fileService.moveImage(member.getProfileImage().getImage()); // 맘에 안듬...
         return MemberSignupResponse.from(member);
     }
 
