42ccd1e6352e23eb11005fd0e3e84fb3800bdd2e
kmw2378 <rlarla677@gmail.com>
2024-12-23 00:00:11 +0900

    [FEAT] MemberService에 프로필 사진, 닉네임 변경 메서드 추가

--- src/main/java/ject/componote/domain/auth/application/MemberService.java
+++ src/main/java/ject/componote/domain/auth/application/MemberService.java
@@ -1,9 +1,16 @@
 package ject.componote.domain.auth.application;
 
-import ject.componote.domain.auth.domain.MemberRepository;
+import ject.componote.domain.auth.domain.Member;
+import ject.componote.domain.auth.dao.MemberRepository;
 import ject.componote.domain.auth.dto.find.response.MemberSummaryResponse;
+import ject.componote.domain.auth.dto.update.request.MemberNicknameUpdateRequest;
+import ject.componote.domain.auth.dto.update.request.MemberProfileImageUpdateRequest;
+import ject.componote.domain.auth.error.DuplicatedNicknameException;
 import ject.componote.domain.auth.error.NotFoundMemberException;
 import ject.componote.domain.auth.model.AuthPrincipal;
+import ject.componote.domain.auth.model.Nickname;
+import ject.componote.domain.auth.model.ProfileImage;
+import ject.componote.infra.file.application.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,12 +19,47 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberService {
+    private final FileService fileService;
     private final MemberRepository memberRepository;
 
     public MemberSummaryResponse getSummary(final AuthPrincipal authPrincipal) {
         final Long memberId = authPrincipal.id();
         return memberRepository.findSummaryById(memberId)
                 .map(MemberSummaryResponse::from)
+                .orElseThrow(() -> NotFoundMemberException.createWhenInvalidMemberId(memberId));
+    }
+
+    @Transactional
+    public void updateProfileImage(final AuthPrincipal authPrincipal, final MemberProfileImageUpdateRequest request) {
+        final Member member = findMemberById(authPrincipal.id());
+        final ProfileImage profileImage = ProfileImage.from(request.profileImageObjectKey());
+        if (member.equalsProfileImage(profileImage)) {
+            return;
+        }
+
+        fileService.moveImage(profileImage.getImage());
+        member.updateProfileImage(profileImage);
+        memberRepository.save(member);
+    }
+
+    @Transactional
+    public void updateNickname(final AuthPrincipal authPrincipal, final MemberNicknameUpdateRequest request) {
+        final Member member = findMemberById(authPrincipal.id());
+        final Nickname nickname = Nickname.from(request.nickname());
+        if (member.equalsNickname(nickname)) {
+            return;
+        }
+
+        if (memberRepository.existsByNickname(nickname)) {
+            throw new DuplicatedNicknameException(nickname);
+        }
+
+        member.updateNickname(nickname);
+        memberRepository.save(member);
+    }
+
+    public Member findMemberById(final Long memberId) {
+        return memberRepository.findById(memberId)
                 .orElseThrow(() -> NotFoundMemberException.createWhenInvalidMemberId(memberId));
     }
 }
