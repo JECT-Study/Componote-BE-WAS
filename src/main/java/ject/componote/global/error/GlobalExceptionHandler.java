8fd909de9457e5bb6ecbeab6e0663f0973e75eca
kmw2378 <rlarla677@gmail.com>
2024-12-23 00:03:15 +0900

    [FEAT] GlobalExceptionHandler에 예외 처리 메서드 추가

--- src/main/java/ject/componote/global/error/GlobalExceptionHandler.java
+++ src/main/java/ject/componote/global/error/GlobalExceptionHandler.java
@@ -1,20 +1,25 @@
 package ject.componote.global.error;
 
+import jakarta.servlet.ServletException;
 import jakarta.validation.ConstraintViolationException;
 import ject.componote.infra.error.InfraException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.sql.SQLException;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.METHOD_NOT_ALLOWED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 @Slf4j
@@ -31,6 +36,24 @@ public class GlobalExceptionHandler {
         log.error("SQL 예외 발생. ", exception);
         return ResponseEntity.status(INTERNAL_SERVER_ERROR)
                 .body(ErrorResponse.of(INTERNAL_SERVER_ERROR, "SQL 오류입니다."));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException exception) {
+        return ResponseEntity.status(METHOD_NOT_ALLOWED)
+                .body(ErrorResponse.of(METHOD_NOT_ALLOWED, exception));
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoResourceFoundException(final NoResourceFoundException exception) {
+        return ResponseEntity.status(NOT_FOUND)
+                .body(ErrorResponse.of(BAD_REQUEST, "URI가 잘못되었습니다."));
+    }
+
+    @ExceptionHandler(ServletException.class)
+    public ResponseEntity<ErrorResponse> handleServerException(final ServletException exception) {
+        return ResponseEntity.status(BAD_REQUEST)
+                .body(ErrorResponse.of(BAD_REQUEST, exception));
     }
 
     @ExceptionHandler(WebClientRequestException.class)
