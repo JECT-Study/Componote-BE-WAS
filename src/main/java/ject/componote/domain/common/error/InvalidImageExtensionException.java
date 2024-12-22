693bc6a13f3b6cf6c626a3901fe26aa2e7413e9c
kmw2378 <rlarla677@gmail.com>
2024-12-22 23:55:47 +0900

    [REFACTOR] 기존 Image 관련 예외 클래스 제거

--- src/main/java/ject/componote/domain/common/error/InvalidImageExtensionException.java
+++
@@ -1,9 +0,0 @@
-package ject.componote.domain.common.error;
-
-import org.springframework.http.HttpStatus;
-
-public class InvalidImageExtensionException extends ImageException {
-    public InvalidImageExtensionException(final String extension) {
-        super("이미지 확장자가 올바르지 않습니다. 입력된 확장자: " + extension, HttpStatus.BAD_REQUEST);
-    }
-}
