6c3369483ceda912a6581b2ba7307093e7e9f356
kmw2378 <rlarla677@gmail.com>
2024-12-22 23:59:34 +0900

    [REFACTOR] MemberSignupRequest 필드명 수정

--- src/main/java/ject/componote/domain/auth/dto/signup/request/MemberSignupRequest.java
+++ src/main/java/ject/componote/domain/auth/dto/signup/request/MemberSignupRequest.java
@@ -3,10 +3,19 @@ package ject.componote.domain.auth.dto.signup.request;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import ject.componote.domain.auth.domain.Member;
 
 public record MemberSignupRequest(
         @NotBlank String nickname,
         @NotBlank String job,
-        @Nullable String profileImageTempKey,
+        @Nullable String profileImageObjectKey,
         @NotNull Long socialAccountId) {
+    public Member toMember() {
+        return Member.of(
+                nickname,
+                job,
+                profileImageObjectKey,
+                socialAccountId
+        );
+    }
 }
