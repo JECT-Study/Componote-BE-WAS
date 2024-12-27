c073e245ae50632fcab390b83c1095fb8ba21a51
kmw2378 <rlarla677@gmail.com>
2024-12-27 17:44:06 +0900

    [REFACTOR] CommentFindByComponentResponse 수정

--- src/main/java/ject/componote/domain/comment/dto/find/response/CommentFindByMemberResponse.java
+++ src/main/java/ject/componote/domain/comment/dto/find/response/CommentFindByMemberResponse.java
@@ -5,13 +5,16 @@ import ject.componote.domain.comment.dao.CommentFindByMemberDao;
 
 import java.time.LocalDateTime;
 
-public record CommentFindByMemberResponse(Long id, @JsonInclude(JsonInclude.Include.NON_NULL) Long parentId, String componentTitle, @JsonInclude(JsonInclude.Include.NON_NULL) String parentContent, String content, LocalDateTime createdAt, boolean isReply) {
+public record CommentFindByMemberResponse(Long id, @JsonInclude(JsonInclude.Include.NON_NULL) Long parentId,
+                                          String componentTitle,
+                                          @JsonInclude(JsonInclude.Include.NON_NULL) String parentContent,
+                                          String content, LocalDateTime createdAt, boolean isReply) {
     public static CommentFindByMemberResponse from(final CommentFindByMemberDao commentFindByMemberDao) {
         return new CommentFindByMemberResponse(
                 commentFindByMemberDao.id(),
-                commentFindByMemberDao.parentId(),
+                commentFindByMemberDao.isReply() ? commentFindByMemberDao.parentId() : null,
                 commentFindByMemberDao.componentTitle(),
-                commentFindByMemberDao.parentContent().getValue(),
+                commentFindByMemberDao.isReply() ? commentFindByMemberDao.parentContent().getValue() : null,
                 commentFindByMemberDao.content().getValue(),
                 commentFindByMemberDao.createdAt(),
                 commentFindByMemberDao.isReply()
