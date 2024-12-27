1e9d900386bec80b5af54232dfd906f4e842f59f
kmw2378 <rlarla677@gmail.com>
2024-12-27 12:46:44 +0900

    [FEAT] CommentFindByMemberResponse 추가

---
+++ src/main/java/ject/componote/domain/comment/dto/find/response/CommentFindByMemberResponse.java
@@ -0,0 +1,20 @@
+package ject.componote.domain.comment.dto.find.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import ject.componote.domain.comment.dao.CommentFindByMemberDao;
+
+import java.time.LocalDateTime;
+
+public record CommentFindByMemberResponse(Long id, @JsonInclude(JsonInclude.Include.NON_NULL) Long parentId, String componentTitle, @JsonInclude(JsonInclude.Include.NON_NULL) String parentContent, String content, LocalDateTime createdAt, boolean isReply) {
+    public static CommentFindByMemberResponse from(final CommentFindByMemberDao commentFindByMemberDao) {
+        return new CommentFindByMemberResponse(
+                commentFindByMemberDao.id(),
+                commentFindByMemberDao.parentId(),
+                commentFindByMemberDao.componentTitle(),
+                commentFindByMemberDao.parentContent().getValue(),
+                commentFindByMemberDao.content().getValue(),
+                commentFindByMemberDao.createdAt(),
+                commentFindByMemberDao.isReply()
+        );
+    }
+}
