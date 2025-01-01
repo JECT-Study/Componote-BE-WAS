deb06d947435e5cce7497e5d4421abf4ed1b2f25
SSUHYUNKIM <yellosuhyun@naver.com>
2025-01-01 22:30:38 +0900

    [REFACTOR] 북마크 에러 수정(#02)

--- src/main/java/ject/componote/domain/bookmark/error/NotFoundBookmarkException.java
+++ src/main/java/ject/componote/domain/bookmark/error/NotFoundBookmarkException.java
@@ -3,7 +3,7 @@ package ject.componote.domain.bookmark.error;
 import org.springframework.http.HttpStatus;
 
 public class NotFoundBookmarkException extends BookmarkException {
-  public NotFoundBookmarkException(final Long memberId, final Long componentId) {
-    super(String.format("해당 회원의 일치하는 북마크를 찾을 수 없습니다. 회원 ID : %d, 컴포넌트 ID : %d", memberId, componentId), HttpStatus.NOT_FOUND);
+  public NotFoundBookmarkException(final Long memberId, final Long resourceId, final String type) {
+    super(String.format("해당 회원의 일치하는 북마크를 찾을 수 없습니다. 회원 ID : %d, %s ID : %d", memberId, type, resourceId), HttpStatus.NOT_FOUND);
   }
 }
