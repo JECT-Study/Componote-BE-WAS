3ac91045fd7fc0d266d65a48869ae8d1c042a215
SSUHYUNKIM <yellosuhyun@naver.com>
2025-01-01 22:29:23 +0900

    [REFACTOR] BookmarkController 수정(#02)

--- src/main/java/ject/componote/domain/bookmark/api/BookmarkController.java
+++ src/main/java/ject/componote/domain/bookmark/api/BookmarkController.java
@@ -1,8 +1,10 @@
 package ject.componote.domain.bookmark.api;
 
+import jakarta.validation.Valid;
 import ject.componote.domain.auth.model.AuthPrincipal;
 import ject.componote.domain.auth.model.Authenticated;
 import ject.componote.domain.bookmark.application.BookmarkService;
+import ject.componote.domain.bookmark.dto.request.BookmarkRequest;
 import ject.componote.domain.bookmark.dto.response.BookmarkResponse;
 import ject.componote.domain.common.dto.response.PageResponse;
 import lombok.RequiredArgsConstructor;
@@ -11,32 +13,34 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/bookmark")
+@RequestMapping("/bookmarks")
 @RequiredArgsConstructor
 @RestController
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
 
-    @PostMapping("{componentId}")
-    public ResponseEntity<BookmarkResponse> addBookmark(@Authenticated final AuthPrincipal authPrincipal, @PathVariable(value = "componentId") final Long componentId ) {
-        return ResponseEntity.ok()
-                .body(bookmarkService.addBookmark(authPrincipal, componentId));
+    @PostMapping
+    public ResponseEntity<BookmarkResponse> addBookmark(
+        @Authenticated AuthPrincipal authPrincipal,
+        @Valid @RequestBody BookmarkRequest bookmarkRequest) {
+    return ResponseEntity.ok(bookmarkService.addBookmark(authPrincipal, bookmarkRequest));
     }
 
     @GetMapping
     public ResponseEntity<PageResponse<BookmarkResponse>> getBookmark(
         @Authenticated final AuthPrincipal authPrincipal,
-        @PageableDefault(size = 10, page = 0) Pageable pageable) {
+        @PageableDefault(size = 10, page = 0) Pageable pageable,
+        @RequestParam(required = false, defaultValue = "component") String type,
+        @RequestParam(required = false, defaultValue = "createdAt") String sortType) {
 
-        return ResponseEntity.ok(bookmarkService.getBookmark(authPrincipal, pageable));
+        return ResponseEntity.ok(
+            bookmarkService.getBookmark(authPrincipal, pageable, type, sortType));
     }
 
-    @DeleteMapping("{componentId}")
-    public ResponseEntity<BookmarkResponse> deleteBookmark(@Authenticated final AuthPrincipal authPrincipal, @PathVariable(value = "componentId") final Long componentId ) {
+    @DeleteMapping
+    public ResponseEntity<BookmarkResponse> deleteBookmark(@Authenticated final AuthPrincipal authPrincipal, @Valid @RequestBody BookmarkRequest bookmarkRequest) {
         return ResponseEntity.ok()
-                .body(bookmarkService.deleteBookmark(authPrincipal, componentId));
+                .body(bookmarkService.deleteBookmark(authPrincipal, bookmarkRequest));
     }
-
-
 }
