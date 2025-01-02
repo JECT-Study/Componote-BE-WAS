074c3e1663281a7697e0d7f03fc96fd6d96dbd38
kmw2378 <rlarla677@gmail.com>
2025-01-02 19:09:00 +0900

    [FEAT] FAQRepository 추가

--- src/main/java/ject/componote/domain/faq/dao/FAQRepository.java
+++ src/main/java/ject/componote/domain/faq/dao/FAQRepository.java
@@ -1,7 +1,11 @@
 package ject.componote.domain.faq.dao;
 
 import ject.componote.domain.faq.domain.FAQ;
+import ject.componote.domain.faq.domain.FAQType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FAQRepository extends JpaRepository<FAQ, Long> {
+    Page<FAQ> findAllByType(final FAQType type, final Pageable pageable);
 }
