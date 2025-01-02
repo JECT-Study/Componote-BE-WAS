4655904d66bc5e6d0cd47c269e9b654019aaed7c
kmw2378 <rlarla677@gmail.com>
2025-01-02 17:13:40 +0900

    [FEAT] NoticeRepository 추가

--- src/main/java/ject/componote/domain/notice/dao/NoticeRepository.java
+++ src/main/java/ject/componote/domain/notice/dao/NoticeRepository.java
@@ -1,7 +1,10 @@
 package ject.componote.domain.notice.dao;
 
 import ject.componote.domain.notice.domain.Notice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    Page<Notice> findAllWithPagination(final Pageable pageable);
 }
