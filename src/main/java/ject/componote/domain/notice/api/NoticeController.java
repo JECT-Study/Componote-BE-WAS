95d3e96d705c4be3b5e0bf20ee0691824dabd8c9
kmw2378 <rlarla677@gmail.com>
2025-01-02 17:28:55 +0900

    [REFACTOR] 누락된 @GetMapping 어노테이션 추가

--- src/main/java/ject/componote/domain/notice/api/NoticeController.java
+++ src/main/java/ject/componote/domain/notice/api/NoticeController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class NoticeController {
     private final NoticeService noticeService;
 
+    @GetMapping
     public ResponseEntity<PageResponse<NoticeResponse>> getNotices(@PageableDefault final Pageable pageable) {
         return ResponseEntity.ok(
                 noticeService.getNotices(pageable)
