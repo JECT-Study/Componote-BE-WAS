b01bfebb845900bc42a979d0059490c2c02316ac
kmw2378 <rlarla677@gmail.com>
2025-01-02 15:28:57 +0900

    [FEAT] ComponentThumbnailConverter 추가

---
+++ src/main/java/ject/componote/domain/component/model/converter/ComponentThumbnailConverter.java
@@ -0,0 +1,18 @@
+package ject.componote.domain.component.model.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import ject.componote.domain.component.model.ComponentThumbnail;
+
+@Converter
+public class ComponentThumbnailConverter implements AttributeConverter<ComponentThumbnail, String> {
+    @Override
+    public String convertToDatabaseColumn(final ComponentThumbnail attribute) {
+        return attribute.getImage().getObjectKey();
+    }
+
+    @Override
+    public ComponentThumbnail convertToEntityAttribute(final String dbData) {
+        return ComponentThumbnail.from(dbData);
+    }
+}
