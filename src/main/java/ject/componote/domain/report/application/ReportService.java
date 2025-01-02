b99b99d4735a2ae389ec3639e53184bfb3c3017b
kmw2378 <rlarla677@gmail.com>
2025-01-02 20:39:24 +0900

    [FEAT] ReportService.create() 검증 로직 추가

--- src/main/java/ject/componote/domain/report/application/ReportService.java
+++ src/main/java/ject/componote/domain/report/application/ReportService.java
@@ -2,12 +2,13 @@ package ject.componote.domain.report.application;
 
 import ject.componote.domain.auth.model.AuthPrincipal;
 import ject.componote.domain.comment.dao.CommentRepository;
+import ject.componote.domain.comment.dto.report.event.CommentReportEvent;
 import ject.componote.domain.comment.error.NotFoundCommentException;
 import ject.componote.domain.report.dao.ReportRepository;
 import ject.componote.domain.report.domain.Report;
 import ject.componote.domain.report.domain.ReportReason;
-import ject.componote.domain.comment.dto.report.event.CommentReportEvent;
 import ject.componote.domain.report.dto.request.ReportRequest;
+import ject.componote.domain.report.error.AlreadyReportedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -23,9 +24,8 @@ public class ReportService {
 
     @Transactional
     public void create(final AuthPrincipal authPrincipal, final Long commentId, final ReportRequest request) {
-        if (!existsCommentById(commentId)) {
-            throw new NotFoundCommentException(commentId);
-        }
+        validateCommentId(commentId);
+        validateAlreadyReported(commentId, commentId);
 
         final Long memberId = authPrincipal.id();
         final ReportReason reason = request.reason();
@@ -33,7 +33,15 @@ public class ReportService {
         eventPublisher.publishEvent(CommentReportEvent.from(commentId));
     }
 
-    private boolean existsCommentById(final Long commentId) {
-        return commentRepository.existsById(commentId);
+    private void validateAlreadyReported(final Long commentId, final Long memberId) {
+        if (reportRepository.existsByCommentIdAndMemberId(commentId, memberId)) {
+            throw new AlreadyReportedException();
+        }
+    }
+
+    private void validateCommentId(final Long commentId) {
+        if (!commentRepository.existsById(commentId)) {
+            throw new NotFoundCommentException(commentId);
+        }
     }
 }
