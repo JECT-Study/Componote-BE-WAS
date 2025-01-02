210a7a5fef9f6966c490a711e09e208d4d52ff9e
kmw2378 <rlarla677@gmail.com>
2025-01-02 15:28:27 +0900

    [FEAT] ComponentImageConverter 추가

---
+++ src/main/java/ject/componote/domain/component/model/converter/ComponentImageConverter.java
@@ -0,0 +1,18 @@
+package ject.componote.domain.component.model.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import ject.componote.domain.component.model.ComponentImage;
+
+@Converter
+public class ComponentImageConverter implements AttributeConverter<ComponentImage, String> {
+    @Override
+    public String convertToDatabaseColumn(final ComponentImage attribute) {
+        return attribute.getImage().getObjectKey();
+    }
+
+    @Override
+    public ComponentImage convertToEntityAttribute(final String dbData) {
+        return ComponentImage.from(dbData);
+    }
+}
