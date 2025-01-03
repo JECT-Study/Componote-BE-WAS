5efde3f2e8f45be333c773ccc2f006b034ecdefd
kmw2378 <rlarla677@gmail.com>
2025-01-03 22:56:56 +0900

    [REFACTOR] Design 관련 Repository 경로 변경 (#44)

--- src/main/java/ject/componote/domain/design/dao/link/DesignLinkRepository.java
+++ src/main/java/ject/componote/domain/design/dao/link/DesignLinkRepository.java
@@ -1,5 +1,6 @@
-package ject.componote.domain.design.domain.link;
+package ject.componote.domain.design.dao.link;
 
+import ject.componote.domain.design.domain.link.DesignLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
