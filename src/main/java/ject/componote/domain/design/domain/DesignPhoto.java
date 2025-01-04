0652c32643b4427a54841bd0575275fca94988f7
kmw2378 <rlarla677@gmail.com>
2025-01-04 14:02:13 +0900

    [REFACTOR] DesignPhoto의 photo 필드 타입 변경 (#44)

--- src/main/java/ject/componote/domain/design/domain/DesignPhoto.java
+++ src/main/java/ject/componote/domain/design/domain/DesignPhoto.java
@@ -6,8 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import ject.componote.domain.common.model.BaseImage;
-import ject.componote.domain.common.model.converter.BaseImageConverter;
+import ject.componote.domain.design.model.DesignPhotoImage;
+import ject.componote.domain.design.model.converter.DesignPhotoImageConverter;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,9 +25,9 @@ public class DesignPhoto {
     @Column(name = "design_id", nullable = false)
     private Long designId;
 
-    @Convert(converter = BaseImageConverter.class)
-    @Column(name = "photo", nullable = false)
-    private BaseImage photo;
+    @Convert(converter = DesignPhotoImageConverter.class)
+    @Column(name = "image", nullable = false)
+    private DesignPhotoImage image;
 
     public void setDesignId(final Long designId) {
         if (designId != null) {
