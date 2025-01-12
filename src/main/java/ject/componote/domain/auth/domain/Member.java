f28542e7a5a6cf4c848bcd6a6f8bb94f3fe07be8
kmw2378 <rlarla677@gmail.com>
2025-01-12 15:48:34 +0900

    [FEAT] Member 엔티티에 equalsEmail(), updateEmail() 메서드 추가 (#51)

--- src/main/java/ject/componote/domain/auth/domain/Member.java
+++ src/main/java/ject/componote/domain/auth/domain/Member.java
@@ -19,7 +19,9 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.DynamicUpdate;
 
+@DynamicUpdate
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -76,11 +78,19 @@ public class Member extends BaseEntity {
         return this.profileImage.equals(profileImage);
     }
 
+    public boolean equalsEmail(final Email email) {
+        return this.email.equals(email);
+    }
+
     public void updateProfileImage(final ProfileImage profileImage) {
         this.profileImage = profileImage;
     }
 
     public void updateNickname(final Nickname nickname) {
         this.nickname = nickname;
+    }
+
+    public void updateEmail(final Email email) {
+        this.email = email;
     }
 }
