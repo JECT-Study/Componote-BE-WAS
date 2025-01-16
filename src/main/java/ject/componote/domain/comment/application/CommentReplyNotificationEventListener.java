44e3d53e1112a9a1e437bd9846d345c9d212a00b
kmw2378 <rlarla677@gmail.com>
2025-01-16 20:32:24 +0900

    [FEAT] CommentReplyNotificationEventListener (#58) - 답글 알림 이벤트를 처리하는 리스너 클래스

---
+++ src/main/java/ject/componote/domain/comment/application/CommentReplyNotificationEventListener.java
@@ -0,0 +1,49 @@
+package ject.componote.domain.comment.application;
+
+import ject.componote.domain.auth.dao.MemberRepository;
+import ject.componote.domain.auth.domain.Member;
+import ject.componote.domain.auth.error.NotFoundMemberException;
+import ject.componote.domain.comment.dao.CommentRepository;
+import ject.componote.domain.comment.domain.Comment;
+import ject.componote.domain.comment.dto.reply.event.CommentReplyNotificationEvent;
+import ject.componote.domain.comment.error.NotFoundCommentException;
+import ject.componote.infra.notification.application.NotificationProducer;
+import ject.componote.infra.notification.dto.create.request.NestedReplyNotificationCreateRequest;
+import ject.componote.infra.notification.dto.create.request.RootReplyNotificationCreateRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CommentReplyNotificationEventListener {
+    private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final NotificationProducer notificationProducer;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleReplyNotificationEvent(final CommentReplyNotificationEvent event) {
+        final Member sender = findMemberById(event.senderId());
+        final Comment parent = findCommentById(event.parentId());
+        final Member receiver = findMemberById(parent.getMemberId());
+        if (commentRepository.isRootComment(parent.getId())) {
+            notificationProducer.sendRootReplyNotification(RootReplyNotificationCreateRequest.of(sender, receiver, parent));
+            return;
+        }
+
+        notificationProducer.sendNestedReplyNotification(NestedReplyNotificationCreateRequest.of(sender, receiver, parent));
+    }
+
+    private Comment findCommentById(final Long commentId) {
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new NotFoundCommentException(commentId));
+    }
+
+    private Member findMemberById(final Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> NotFoundMemberException.createWhenInvalidMemberId(memberId));
+    }
+}
