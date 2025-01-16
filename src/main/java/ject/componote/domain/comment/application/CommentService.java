169f7e19f82400f270eb7dc3002dcb733b5065ff
kmw2378 <rlarla677@gmail.com>
2025-01-16 20:36:55 +0900

    [REFACTOR] 대댓글 생성 시 알림 이벤트 발생 로직 추가 (#58)

--- src/main/java/ject/componote/domain/comment/application/CommentService.java
+++ src/main/java/ject/componote/domain/comment/application/CommentService.java
@@ -3,7 +3,6 @@ package ject.componote.domain.comment.application;
 import ject.componote.domain.auth.model.AuthPrincipal;
 import ject.componote.domain.comment.dao.CommentFindByComponentDao;
 import ject.componote.domain.comment.dao.CommentFindByParentDao;
-import ject.componote.domain.comment.dao.CommentLikeRepository;
 import ject.componote.domain.comment.dao.CommentRepository;
 import ject.componote.domain.comment.domain.Comment;
 import ject.componote.domain.comment.dto.create.request.CommentCreateRequest;
@@ -11,12 +10,9 @@ import ject.componote.domain.comment.dto.create.response.CommentCreateResponse;
 import ject.componote.domain.comment.dto.find.response.CommentFindByComponentResponse;
 import ject.componote.domain.comment.dto.find.response.CommentFindByMemberResponse;
 import ject.componote.domain.comment.dto.find.response.CommentFindByParentResponse;
-import ject.componote.domain.comment.dto.like.event.CommentLikeEvent;
-import ject.componote.domain.comment.dto.like.event.CommentUnlikeEvent;
 import ject.componote.domain.comment.dto.reply.event.CommentReplyCountIncreaseEvent;
+import ject.componote.domain.comment.dto.reply.event.CommentReplyNotificationEvent;
 import ject.componote.domain.comment.dto.update.request.CommentUpdateRequest;
-import ject.componote.domain.comment.error.AlreadyLikedException;
-import ject.componote.domain.comment.error.NoLikedException;
 import ject.componote.domain.comment.error.NotFoundCommentException;
 import ject.componote.domain.comment.error.NotFoundParentCommentException;
 import ject.componote.domain.comment.model.CommentContent;
@@ -45,8 +41,12 @@ public class CommentService {
         final Comment comment = commentRepository.save(
                 CommentCreationStrategy.createBy(request, authPrincipal.id())
         );
-        increaseParentReplyCount(request);
         fileService.moveImage(comment.getImage());
+
+        if (isReply(request)) {
+            eventPublisher.publishEvent(CommentReplyCountIncreaseEvent.from(comment));
+            eventPublisher.publishEvent(CommentReplyNotificationEvent.from(comment));
+        }
         return CommentCreateResponse.from(comment);
     }
 
@@ -66,8 +66,8 @@ public class CommentService {
     }
 
     public PageResponse<CommentFindByParentResponse> getRepliesByComponentId(final AuthPrincipal authPrincipal,
-                                                   final Long parentId,
-                                                   final Pageable pageable) {
+                                                                             final Long parentId,
+                                                                             final Pageable pageable) {
         final Page<CommentFindByParentResponse> page = findCommentsByParentId(authPrincipal, parentId, pageable)
                 .map(CommentFindByParentResponse::from);
         return PageResponse.from(page);
@@ -85,8 +85,6 @@ public class CommentService {
 
         final CommentContent content = CommentContent.from(commentUpdateRequest.content()); // 가비지가 발생하지 않을까?
         comment.update(content, image);
-
-        commentRepository.save(comment);
     }
 
     @CommenterValidation
@@ -133,18 +131,5 @@ public class CommentService {
         if (!commentRepository.existsById(parentId)) {
             throw new NotFoundParentCommentException(parentId);
         }
-    }
-
-    private void increaseParentReplyCount(final CommentCreateRequest request) {
-        if (!isReply(request)) {
-            return;
-        }
-
-        final Long parentId = request.parentId();
-        eventPublisher.publishEvent(CommentReplyCountIncreaseEvent.from(parentId));
-    }
-
-    private boolean isAlreadyLiked(final Long commentId, final Long memberId) {
-        return commentLikeRepository.existsByCommentIdAndMemberId(commentId, memberId);
     }
 }
