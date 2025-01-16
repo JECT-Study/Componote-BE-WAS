1ac427058650bb8af1d5261372cc72012f91d00c
kmw2378 <rlarla677@gmail.com>
2025-01-16 22:02:04 +0900

    [FEAT] 대댓글 관련 Event 클래스 from() 메서드 파라미터 명을 reply로 수정 (#58)

--- src/main/java/ject/componote/domain/comment/dto/reply/event/CommentReplyCountIncreaseEvent.java
+++ src/main/java/ject/componote/domain/comment/dto/reply/event/CommentReplyCountIncreaseEvent.java
@@ -3,7 +3,7 @@ package ject.componote.domain.comment.dto.reply.event;
 import ject.componote.domain.comment.domain.Comment;
 
 public record CommentReplyCountIncreaseEvent(Long parentId) {
-    public static CommentReplyCountIncreaseEvent from(final Comment comment) {
-        return new CommentReplyCountIncreaseEvent(comment.getParentId());
+    public static CommentReplyCountIncreaseEvent from(final Comment reply) {
+        return new CommentReplyCountIncreaseEvent(reply.getParentId());
     }
 }
--- src/main/java/ject/componote/domain/comment/dto/reply/event/CommentReplyNotificationEvent.java
+++ src/main/java/ject/componote/domain/comment/dto/reply/event/CommentReplyNotificationEvent.java
@@ -3,7 +3,7 @@ package ject.componote.domain.comment.dto.reply.event;
 import ject.componote.domain.comment.domain.Comment;
 
 public record CommentReplyNotificationEvent(Long senderId, Long parentId, Long commentId) {
-    public static CommentReplyNotificationEvent from(final Comment comment) {
-        return new CommentReplyNotificationEvent(comment.getMemberId(), comment.getParentId(), comment.getId());
+    public static CommentReplyNotificationEvent from(final Comment reply) {
+        return new CommentReplyNotificationEvent(reply.getMemberId(), reply.getParentId(), reply.getId());
     }
 }
