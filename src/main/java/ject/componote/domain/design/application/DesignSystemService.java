53e6b3e96037720fcea2f20c9dbb80b4e9a218d1
SSUHYUNKIM <yellosuhyun@naver.com>
2025-01-19 08:46:09 +0900

    [FEAT] DesignSystemService 작업(#41)

--- src/main/java/ject/componote/domain/design/application/DesignSystemService.java
+++ src/main/java/ject/componote/domain/design/application/DesignSystemService.java
@@ -1,5 +1,13 @@
 package ject.componote.domain.design.application;
 
+import ject.componote.domain.auth.model.AuthPrincipal;
+import ject.componote.domain.common.dto.response.PageResponse;
+import ject.componote.domain.component.application.ComponentSearchStrategy;
+import ject.componote.domain.component.domain.Component;
+import ject.componote.domain.component.dto.find.event.ComponentViewCountIncreaseEvent;
+import ject.componote.domain.component.dto.find.request.ComponentSearchRequest;
+import ject.componote.domain.component.dto.find.response.ComponentDetailResponse;
+import ject.componote.domain.component.dto.find.response.ComponentSummaryResponse;
 import ject.componote.domain.design.domain.Design;
 import ject.componote.domain.design.domain.filter.DesignFilter;
 import ject.componote.domain.design.dao.filter.DesignFilterRepository;
@@ -7,7 +15,11 @@ import ject.componote.domain.design.dao.DesignRepository;
 import ject.componote.domain.design.domain.DesignSystem;
 import ject.componote.domain.design.domain.link.DesignLink;
 import ject.componote.domain.design.dao.link.DesignLinkRepository;
+import ject.componote.domain.design.dto.search.request.DesignSystemSearchRequest;
+import ject.componote.domain.design.dto.search.response.DesignSystemSearchResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,15 +29,23 @@ import java.util.List;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class DesignSystemService {
+
     private final DesignRepository designRepository;
     private final DesignFilterRepository designFilterRepository;
     private final DesignLinkRepository designLinkRepository;
 
-    private DesignSystem createDesignSystem(final Long designId) {
-        final Design design = designRepository.findById(designId)
-                .orElseThrow(IllegalArgumentException::new);
-        final List<DesignFilter> filters = designFilterRepository.findAllByDesignId(designId);
-        final List<DesignLink> links = designLinkRepository.findAllByDesignId(designId);
-        return DesignSystem.of(design, links, filters);
+    public PageResponse<DesignSystemSearchResponse> searchDesignSystem(final AuthPrincipal authPrincipal,
+                                                                       final DesignSystemSearchRequest request,
+                                                                       final Pageable pageable) {
+        final Page<Design> designs = DesignSystemSearchStrategy.searchBy(authPrincipal, designRepository, request, pageable);
+
+        final Page<DesignSystemSearchResponse> responsePage = designs.map(design -> {
+            List<DesignFilter> filters = designFilterRepository.findAllByDesignId(design.getId());
+            List<DesignLink> links = designLinkRepository.findAllByDesignId(design.getId());
+            DesignSystem designSystem = DesignSystem.of(design, links, filters);
+            return DesignSystemSearchResponse.from(designSystem);
+        });
+
+        return PageResponse.from(responsePage);
     }
 }
