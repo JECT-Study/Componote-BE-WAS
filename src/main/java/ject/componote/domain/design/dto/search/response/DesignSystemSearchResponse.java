3b94f90f250229dc4163cc9e0cbe668bb12ddbbc
SSUHYUNKIM <yellosuhyun@naver.com>
2025-01-19 08:46:18 +0900

    [FEAT] DesignSystemSearchResponse 작업(#41)

--- src/main/java/ject/componote/domain/design/dto/search/response/DesignSystemSearchResponse.java
+++ src/main/java/ject/componote/domain/design/dto/search/response/DesignSystemSearchResponse.java
@@ -1,6 +1,31 @@
 package ject.componote.domain.design.dto.search.response;
 
-import java.util.List;
+import ject.componote.domain.design.domain.DesignSystem;
 
-public record DesignSystemSearchResponse(String name, String organizationName, String description, List<DesignFilterSearchResponse> filters) {
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public record DesignSystemSearchResponse(
+        String name,
+        String organizationName,
+        String description,
+        List<DesignFilterSearchResponse> filters,
+        Map<String, String> links
+) {
+    public static DesignSystemSearchResponse from(DesignSystem designSystem) {
+        return new DesignSystemSearchResponse(
+                designSystem.getDesign().getSummary().getName(),
+                designSystem.getDesign().getSummary().getOrganization(),
+                designSystem.getDesign().getSummary().getDescription(),
+                designSystem.getFilters().getFilters().stream()
+                        .map(filter -> new DesignFilterSearchResponse(filter.getType().name(), List.of(filter.getValue())))
+                        .collect(Collectors.toList()),
+                designSystem.getLinks().getLinks().stream()
+                        .collect(Collectors.toMap(
+                                link -> link.getType().name().toLowerCase(),
+                                link -> link.getUrl().getValue()
+                        ))
+        );
+    }
 }
