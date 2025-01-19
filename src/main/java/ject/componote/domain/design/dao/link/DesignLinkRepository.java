1ce580bf42afe818bdb8523368e1686cf5703000
SSUHYUNKIM <yellosuhyun@naver.com>
2025-01-19 09:10:05 +0900

    [FEAT] DesignLinkRepository 작업(#41)

--- src/main/java/ject/componote/domain/design/dao/link/DesignLinkRepository.java
+++ src/main/java/ject/componote/domain/design/dao/link/DesignLinkRepository.java
@@ -1,7 +1,9 @@
 package ject.componote.domain.design.dao.link;
 
+import ject.componote.domain.design.domain.filter.DesignFilter;
 import ject.componote.domain.design.domain.link.DesignLink;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
