6fa61fe787b6f523b6ec4c1c391681e2b0b88822
kmw2378 <rlarla677@gmail.com>
2025-01-22 15:39:26 +0900

    [FEAT] AuthService에 @Transactional 어노테이션 추가 (#65)

--- src/main/java/ject/componote/domain/auth/application/AuthService.java
+++ src/main/java/ject/componote/domain/auth/application/AuthService.java
@@ -18,15 +18,18 @@ import ject.componote.domain.auth.util.TokenProvider;
 import ject.componote.infra.storage.application.StorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class AuthService {
     private final StorageService storageService;
     private final MemberRepository memberRepository;
     private final SocialAccountRepository socialAccountRepository;
     private final TokenProvider tokenProvider;
 
+    @Transactional
     public MemberSignupResponse signup(final MemberSignupRequest request) {
         final Long socialAccountId = request.socialAccountId();
         if (!socialAccountRepository.existsById(socialAccountId)) {
