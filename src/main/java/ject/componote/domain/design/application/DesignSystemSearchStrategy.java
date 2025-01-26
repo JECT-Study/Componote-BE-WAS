145891b2d044942679b16bbbd5d4ba697da1ffa9
SSUHYUNKIM <yellosuhyun@naver.com>
2025-01-26 18:23:20 +0900

    [REFACTOR] 하나의 SQL로 처리(#41)

--- src/main/java/ject/componote/domain/design/application/DesignSystemSearchStrategy.java
+++ src/main/java/ject/componote/domain/design/application/DesignSystemSearchStrategy.java
@@ -1,10 +1,6 @@
 package ject.componote.domain.design.application;
 
 import ject.componote.domain.auth.model.AuthPrincipal;
-import ject.componote.domain.component.dao.ComponentRepository;
-import ject.componote.domain.component.dao.ComponentSummaryDao;
-import ject.componote.domain.component.dto.find.request.ComponentSearchRequest;
-import ject.componote.domain.component.error.InvalidCommentSearchStrategyException;
 import ject.componote.domain.design.dao.DesignRepository;
 import ject.componote.domain.design.dao.filter.DesignFilterRepository;
 import ject.componote.domain.design.dao.link.DesignLinkRepository;
@@ -19,90 +15,89 @@ import org.springframework.data.domain.Pageable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public enum DesignSystemSearchStrategy {
-    WITH_BOOKMARK_AND_FILTER(
-            (authPrincipal, request) -> isLoggedIn(authPrincipal) && hasFilter(request),
-            (authPrincipal, designRepository, filterRepository, linkRepository, request, pageable) -> {
-                List<Long> designIds = request.filters().stream()
-                        .flatMap(filter -> {
-                            FilterType filterType = FilterType.valueOf(filter.type().toUpperCase());
-                            return filterRepository.findAllDesignIdByCondition(filterType, filter.values()).stream();
-                        })
-                        .distinct()
-                        .toList();
+  WITH_BOOKMARK_AND_FILTER(
+      (authPrincipal, request) -> isLoggedIn(authPrincipal) && hasFilter(request),
+      (authPrincipal, designRepository, filterRepository, linkRepository, request, pageable) ->
+          findDesignsWithFilters(authPrincipal.id(), true, request, designRepository, filterRepository, pageable)
+  ),
 
-                if (designIds.isEmpty()) {
-                    return Page.empty();
-                }
+  WITH_BOOKMARK(
+      (authPrincipal, request) -> isLoggedIn(authPrincipal) && !hasFilter(request),
+      (authPrincipal, designRepository, filterRepository, linkRepository, request, pageable) ->
+          designRepository.findByKeywordAndBookmarkStatus(authPrincipal.id(), request.keyword(), pageable)
+  ),
 
-                return designRepository.findAllByIdInAndBookmarkStatus(authPrincipal.id(), designIds, pageable);
-            }
-    ),
+  WITHOUT_BOOKMARK_AND_FILTER(
+      (authPrincipal, request) -> !isLoggedIn(authPrincipal) && hasFilter(request),
+      (authPrincipal, designRepository, filterRepository, linkRepository, request, pageable) ->
+          findDesignsWithFilters(null, false, request, designRepository, filterRepository, pageable)
+  ),
 
-    WITH_BOOKMARK(
-            (authPrincipal, request) -> isLoggedIn(authPrincipal) && !hasFilter(request),
-            (authPrincipal, designRepository, filterRepository, linkRepository, request, pageable) ->
-                    designRepository.findByKeywordAndBookmarkStatus(authPrincipal.id(), request.keyword(), pageable)
-    ),
+  WITHOUT_BOOKMARK(
+      (authPrincipal, request) -> !isLoggedIn(authPrincipal) && !hasFilter(request),
+      (authPrincipal, designRepository, filterRepository, linkRepository, request, pageable) ->
+          designRepository.findByKeyword(request.keyword(), pageable)
+  );
 
-    WITHOUT_BOOKMARK_AND_FILTER(
-            (authPrincipal, request) -> !isLoggedIn(authPrincipal) && hasFilter(request),
-            (authPrincipal, designRepository, filterRepository, linkRepository, request, pageable) -> {
-                List<Long> designIds = request.filters().stream()
-                        .flatMap(filter -> {
-                            FilterType filterType = FilterType.valueOf(filter.type().toUpperCase());
-                            return filterRepository.findAllDesignIdByCondition(filterType, filter.values()).stream();
-                        })
-                        .distinct()
-                        .toList();
+  private final BiPredicate<AuthPrincipal, DesignSystemSearchRequest> condition;
+  private final DesignSystemSearchFunction searchFunction;
 
-                if (designIds.isEmpty()) {
-                    return Page.empty();
-                }
+  public static Page<Design> searchBy(final AuthPrincipal authPrincipal,
+      final DesignRepository designRepository,
+      final DesignFilterRepository filterRepository,
+      final DesignLinkRepository linkRepository,
+      final DesignSystemSearchRequest request,
+      final Pageable pageable) {
+    return Arrays.stream(values())
+        .filter(strategy -> strategy.condition.test(authPrincipal, request))
+        .findFirst()
+        .orElseThrow(NotFoundDesignException::new)
+        .searchFunction.search(authPrincipal, designRepository, filterRepository, linkRepository, request, pageable);
+  }
 
-                return designRepository.findAllByIdIn(designIds, pageable);
-            }
-    ),
+  private static boolean isLoggedIn(final AuthPrincipal authPrincipal) {
+    return authPrincipal != null;
+  }
 
-    WITHOUT_BOOKMARK(
-            (authPrincipal, request) -> !isLoggedIn(authPrincipal) && !hasFilter(request),
-            (authPrincipal, designRepository, filterRepository, linkRepository, request, pageable) ->
-                    designRepository.findByKeyword(request.keyword(), pageable)
-    );
+  private static boolean hasFilter(final DesignSystemSearchRequest request) {
+    return request.filters() != null && !request.filters().isEmpty();
+  }
 
-    private final BiPredicate<AuthPrincipal, DesignSystemSearchRequest> condition;
-    private final DesignSystemSearchFunction searchFunction;
+  private static Page<Design> findDesignsWithFilters(Long memberId, boolean includeBookmark,
+      DesignSystemSearchRequest request,
+      DesignRepository designRepository,
+      DesignFilterRepository filterRepository,
+      Pageable pageable) {
+    List<Long> designIds = request.filters().stream()
+        .flatMap(filter -> {
+          FilterType filterType = FilterType.valueOf(filter.type().toUpperCase());
+          return filterRepository.findAllDesignIdByCondition(filterType, filter.values()).stream();
+        })
+        .distinct()
+        .collect(Collectors.toList());
 
-    public static Page<Design> searchBy(final AuthPrincipal authPrincipal,
-                                        final DesignRepository designRepository,
-                                        final DesignFilterRepository filterRepository,
-                                        final DesignLinkRepository linkRepository,
-                                        final DesignSystemSearchRequest request,
-                                        final Pageable pageable) {
-        return Arrays.stream(values())
-                .filter(strategy -> strategy.condition.test(authPrincipal, request))
-                .findFirst()
-                .orElseThrow(NotFoundDesignException::new)
-                .searchFunction.search(authPrincipal, designRepository, filterRepository, linkRepository, request, pageable);
+    if (designIds.isEmpty()) {
+      return Page.empty();
     }
 
-    private static boolean isLoggedIn(final AuthPrincipal authPrincipal) {
-        return authPrincipal != null;
+    if (includeBookmark && memberId != null) {
+      return designRepository.findAllByIdInAndBookmarkStatus(memberId, designIds, pageable);
     }
 
-    private static boolean hasFilter(final DesignSystemSearchRequest request) {
-        return request.filters() != null && !request.filters().isEmpty();
-    }
+    return designRepository.findAllByIdIn(designIds, pageable);
+  }
 
-    @FunctionalInterface
-    private interface DesignSystemSearchFunction {
-        Page<Design> search(final AuthPrincipal authPrincipal,
-                            final DesignRepository designRepository,
-                            final DesignFilterRepository filterRepository,
-                            final DesignLinkRepository linkRepository,
-                            final DesignSystemSearchRequest request,
-                            final Pageable pageable);
-    }
+  @FunctionalInterface
+  private interface DesignSystemSearchFunction {
+    Page<Design> search(final AuthPrincipal authPrincipal,
+        final DesignRepository designRepository,
+        final DesignFilterRepository filterRepository,
+        final DesignLinkRepository linkRepository,
+        final DesignSystemSearchRequest request,
+        final Pageable pageable);
+  }
 }
