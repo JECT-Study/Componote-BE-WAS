cd9b04b441a7ef49e9cd405233cc940312533713
SSUHYUNKIM <yellosuhyun@naver.com>
2025-01-26 18:13:31 +0900

    [FIX] 에러 상태코드 변경(#41)

--- src/main/java/ject/componote/domain/design/error/NotFoundDesignException.java
+++ src/main/java/ject/componote/domain/design/error/NotFoundDesignException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public class NotFoundDesignException extends DesignException {
     public NotFoundDesignException() {
-        super("디자인이 없습니다.", HttpStatus.BAD_REQUEST);
+        super("디자인이 없습니다.", HttpStatus.NOT_FOUND);
     }
 }
