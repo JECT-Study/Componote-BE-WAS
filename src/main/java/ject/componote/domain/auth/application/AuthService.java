479736145d7b5dd130bff9bc2bc4ddc6ab82163d
kmw2378 <rlarla677@gmail.com>
2025-01-27 17:22:52 +0900

    [REFACTOR] 소셜 로그인, 회원가입/로그인 시 토큰 생성 로직 수정 (#68)

--- src/main/java/ject/componote/domain/auth/application/AuthService.java
+++ src/main/java/ject/componote/domain/auth/application/AuthService.java
@@ -14,8 +14,7 @@ import ject.componote.domain.auth.error.NotFoundMemberException;
 import ject.componote.domain.auth.error.NotFoundSocialAccountException;
 import ject.componote.domain.auth.model.AuthPrincipal;
 import ject.componote.domain.auth.model.Nickname;
-import ject.componote.domain.auth.util.AESCryptography;
-import ject.componote.domain.auth.util.TokenProvider;
+import ject.componote.domain.auth.token.application.TokenService;
 import ject.componote.infra.storage.application.StorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,15 +24,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true)
 public class AuthService {
-    private final AESCryptography aesCryptography;
     private final StorageService storageService;
     private final MemberRepository memberRepository;
     private final SocialAccountRepository socialAccountRepository;
-    private final TokenProvider tokenProvider;
+    private final TokenService tokenService;
 
     @Transactional
     public MemberSignupResponse signup(final MemberSignupRequest request) {
-        final Long socialAccountId = parseSocialAccountId(request.encryptedSocialAccountId());
+        final Long socialAccountId = getSocialAccountId(request.socialAccountToken());
         if (!socialAccountRepository.existsById(socialAccountId)) {
             throw new NotFoundSocialAccountException();
         }
@@ -43,20 +41,16 @@ public class AuthService {
         }
 
         final Member member = memberRepository.save(request.toMember(socialAccountId));
-        final String accessToken = tokenProvider.createToken(AuthPrincipal.from(member));
         storageService.moveImage(member.getProfileImage());
+        final String accessToken = createAccessToken(member);
         return MemberSignupResponse.of(accessToken, member);
     }
 
     public MemberLoginResponse login(final MemberLoginRequest request) {
-        final Long socialAccountId = parseSocialAccountId(request.encryptedSocialAccountId());
+        final Long socialAccountId = getSocialAccountId(request.socialAccountToken());
         final Member member = findMemberBySocialAccountId(socialAccountId);
-        final String accessToken = tokenProvider.createToken(AuthPrincipal.from(member));
+        final String accessToken = createAccessToken(member);
         return MemberLoginResponse.of(accessToken, member);
-    }
-
-    private Long parseSocialAccountId(final String encryptedSocialAccountId) {
-        return aesCryptography.decrypt(encryptedSocialAccountId, Long::valueOf);
     }
 
     public void validateNickname(final MemberNicknameValidateRequest request) {
@@ -69,5 +63,13 @@ public class AuthService {
     private Member findMemberBySocialAccountId(final Long socialAccountId) {
         return memberRepository.findBySocialAccountId(socialAccountId)
                 .orElseThrow(NotFoundMemberException::createWhenInvalidSocialAccountId);
+    }
+
+    private String createAccessToken(final Member member) {
+        return tokenService.createAccessToken(AuthPrincipal.from(member));
+    }
+
+    private Long getSocialAccountId(final String socialAccountToken) {
+        return tokenService.extractSocialAccountTokenPayload(socialAccountToken);
     }
 }
--- src/main/java/ject/componote/domain/auth/application/OAuthService.java
+++ src/main/java/ject/componote/domain/auth/application/OAuthService.java
@@ -2,22 +2,24 @@ package ject.componote.domain.auth.application;
 
 import ject.componote.domain.auth.dao.MemberRepository;
 import ject.componote.domain.auth.domain.SocialAccount;
-import ject.componote.domain.auth.dto.login.response.OAuthLoginResponse;
 import ject.componote.domain.auth.dto.authorize.response.OAuthAuthorizationUrlResponse;
-import ject.componote.domain.auth.util.AESCryptography;
+import ject.componote.domain.auth.dto.login.response.OAuthLoginResponse;
+import ject.componote.domain.auth.token.application.TokenService;
 import ject.componote.infra.oauth.application.OAuthClient;
 import ject.componote.infra.oauth.dto.authorize.response.OAuthAuthorizePayload;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.scheduler.Schedulers;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OAuthService {
-    private final AESCryptography aesCryptography;
     private final MemberRepository memberRepository;
     private final OAuthClient oAuthClient;
     private final OAuthResultHandler oauthResultHandler;
+    private final TokenService tokenService;
 
     public OAuthAuthorizationUrlResponse getOAuthAuthorizationCodeUrl(final String providerType) {
         final OAuthAuthorizePayload oAuthAuthorizePayload = oAuthClient.getAuthorizePayload(providerType);
@@ -29,8 +31,8 @@ public class OAuthService {
                 .publishOn(Schedulers.boundedElastic())
                 .map(oauthResultHandler::saveOrGet)
                 .block();
-        final Long socialAccountId = socialAccount.getId();
-        final boolean isRegister = memberRepository.existsBySocialAccountId(socialAccountId);
-        return OAuthLoginResponse.of(isRegister, aesCryptography.encrypt(socialAccountId));
+        final boolean isRegister = memberRepository.existsBySocialAccountId(socialAccount.getId());
+        final String socialAccountToken = tokenService.createSocialAccountToken(socialAccount);
+        return OAuthLoginResponse.of(isRegister, socialAccountToken);
     }
 }
