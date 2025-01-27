cae339016d2bb5f7f4c97afd3874667a49be05a9
kmw2378 <rlarla677@gmail.com>
2025-01-27 17:19:14 +0900

    [FEAT] InvalidSocialAccountTokenException 추가 (#68)

---
+++ src/main/java/ject/componote/domain/auth/token/error/InvalidSocialAccountTokenException.java
@@ -0,0 +1,13 @@
+package ject.componote.domain.auth.token.error;
+
+import ject.componote.domain.auth.error.AuthException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+
+@Slf4j
+public class InvalidSocialAccountTokenException extends AuthException {
+    public InvalidSocialAccountTokenException(final String socialAccountToken) {
+        super("유효하지 않은 socialAccountToken 입니다.", HttpStatus.BAD_REQUEST);
+        log.error("socialAccountToken: {}", socialAccountToken);
+    }
+}
