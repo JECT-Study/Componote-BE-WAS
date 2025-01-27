a90e5f4f1479471c02d38eb65df45b80ff138f0a
kmw2378 <rlarla677@gmail.com>
2025-01-27 17:18:52 +0900

    [FEAT] TokenProvider 추가 (#68) - 토큰 관련 설정 파일 정보를 매핑해주는 클래스 추가

---
+++ src/main/java/ject/componote/domain/auth/token/model/TokenProvider.java
@@ -0,0 +1,15 @@
+package ject.componote.domain.auth.token.model;
+
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+
+public record TokenProvider(Long expiration, Key key) {
+    public static TokenProvider from(final TokenProperties.Token attribute) {
+        return new TokenProvider(
+                attribute.getExpiration(),
+                Keys.hmacShaKeyFor(attribute.getSecretKey().getBytes(StandardCharsets.UTF_8))
+        );
+    }
+}
