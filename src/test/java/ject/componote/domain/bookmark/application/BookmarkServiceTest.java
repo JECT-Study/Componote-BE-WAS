74283b71307ec2a0752eda6c76247aab9e8a6759
SSUHYUNKIM <yellosuhyun@naver.com>
2025-02-02 20:31:02 +0900

    [TEST] 테스트 코드 수정(#02)

--- src/test/java/ject/componote/domain/bookmark/application/BookmarkServiceTest.java
+++ src/test/java/ject/componote/domain/bookmark/application/BookmarkServiceTest.java
@@ -3,10 +3,10 @@ package ject.componote.domain.bookmark.application;
 import ject.componote.domain.auth.dao.MemberRepository;
 import ject.componote.domain.auth.domain.Member;
 import ject.componote.domain.auth.model.AuthPrincipal;
-import ject.componote.domain.bookmark.dao.BookmarkRepository;
 import ject.componote.domain.bookmark.dao.ComponentBookmarkRepository;
 import ject.componote.domain.bookmark.domain.ComponentBookmark;
 import ject.componote.domain.bookmark.dto.request.BookmarkRequest;
+import ject.componote.domain.bookmark.dto.response.ComponentBookmarkResponse;
 import ject.componote.domain.bookmark.error.NotFoundBookmarkException;
 import ject.componote.domain.common.dto.response.PageResponse;
 import ject.componote.domain.component.dao.ComponentRepository;
@@ -37,9 +37,6 @@ import static org.mockito.Mockito.*;
 class BookmarkServiceTest {
 
   @Mock
-  BookmarkRepository bookmarkRepository;
-
-  @Mock
   MemberRepository memberRepository;
 
   @Mock
@@ -66,7 +63,6 @@ class BookmarkServiceTest {
     // given
     Component component = ComponentFixture.INPUT_COMPONENT.생성();
     final Long componentId = component.getId();
-    BookmarkRequest request = new BookmarkRequest(componentId, "component");
 
     when(memberRepository.findById(member.getId()))
             .thenReturn(Optional.of(member));
@@ -76,16 +72,16 @@ class BookmarkServiceTest {
             .thenAnswer(invocation -> invocation.getArgument(0));
 
     // when
-    BookmarkResponse response = bookmarkService.addBookmark(authPrincipal, request);
+    ComponentBookmarkResponse response = bookmarkService.addComponentBookmark(authPrincipal, componentId);
 
     // then
-    assertThat(response.resourceId()).isEqualTo(componentId);
+    assertThat(response.componentId()).isEqualTo(componentId);
     verify(componentBookmarkRepository).save(any(ComponentBookmark.class));
   }
 
   @Test
-  @DisplayName("북마크 조회 성공")
-  public void getBookmark_Success() {
+  @DisplayName("컴포넌트 북마크 조회 성공")
+  public void getComponentBookmarks_Success() {
     // given
     PageRequest pageable = PageRequest.of(0, 10, Sort.by("createdAt").descending());
     Component component = ComponentFixture.INPUT_COMPONENT.생성();
@@ -98,21 +94,19 @@ class BookmarkServiceTest {
             .thenReturn(Optional.of(component));
 
     // when
-    PageResponse<BookmarkResponse> response = bookmarkService.getBookmarks(authPrincipal, pageable, "component");
+    PageResponse<ComponentBookmarkResponse> response = bookmarkService.getComponentBookmarks(authPrincipal, pageable);
 
     // then
     assertThat(response.getTotalElements()).isEqualTo(1);
     verify(componentBookmarkRepository).findAllByMemberId(eq(authPrincipal.id()), eq(pageable));
   }
 
-
   @Test
-  @DisplayName("북마크 삭제 성공")
-  public void deleteBookmark_Success() {
+  @DisplayName("컴포넌트 북마크 삭제 성공")
+  public void deleteComponentBookmark_Success() {
     // given
     Component component = ComponentFixture.INPUT_COMPONENT.생성();
     final Long componentId = component.getId();
-    BookmarkRequest request = new BookmarkRequest(componentId, "component");
     ComponentBookmark bookmark = BookmarkFixture.COMPONENT_BOOKMARK.컴포넌트_북마크_생성(member, component);
 
     when(componentRepository.findById(componentId))
@@ -121,25 +115,23 @@ class BookmarkServiceTest {
             .thenReturn(Optional.of(bookmark));
 
     // when
-    bookmarkService.deleteBookmark(authPrincipal, request);
+    bookmarkService.deleteComponentBookmark(authPrincipal, componentId);
 
     // then
     verify(componentBookmarkRepository).delete(bookmark);
   }
 
   @Test
-  @DisplayName("북마크 삭제 시 존재하지 않을 경우 예외 발생")
-  public void deleteBookmark_NotFound() {
+  @DisplayName("컴포넌트 북마크 삭제 시 존재하지 않을 경우 예외 발생")
+  public void deleteComponentBookmark_NotFound() {
     // given
     Long componentId = 1L;
-    BookmarkRequest request = new BookmarkRequest(componentId, "component");
 
-    // Mock 설정
     when(componentBookmarkRepository.findByMemberIdAndComponentId(member.getId(), componentId))
             .thenReturn(Optional.empty());
 
     // then
-    assertThatThrownBy(() -> bookmarkService.deleteBookmark(authPrincipal, request))
-        .isInstanceOf(NotFoundBookmarkException.class);
+    assertThatThrownBy(() -> bookmarkService.deleteComponentBookmark(authPrincipal, componentId))
+            .isInstanceOf(NotFoundBookmarkException.class);
   }
 }
