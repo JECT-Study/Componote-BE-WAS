5236e67afe023783931d26ca9e3d9c87d17f806a
kmw2378 <rlarla677@gmail.com>
2025-02-07 21:51:27 +0900

    [REFACTOR] NotFoundParentCommentException 예외 메시지 수정 (#95)

--- src/main/java/ject/componote/domain/comment/error/NotFoundParentCommentException.java
+++ src/main/java/ject/componote/domain/comment/error/NotFoundParentCommentException.java
@@ -3,7 +3,7 @@ package ject.componote.domain.comment.error;
 import org.springframework.http.HttpStatus;
 
 public class NotFoundParentCommentException extends CommentException {
-    public NotFoundParentCommentException(final Long parentId) {
-        super("일치하는 부모 댓글을 찾을 수 없습니다. 댓글 ID: " + parentId, HttpStatus.NOT_FOUND);
+    public NotFoundParentCommentException() {
+        super("컴포넌트에 부모 댓글이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
     }
 }
