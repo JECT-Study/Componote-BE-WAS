04b806131d77488c0ccc556227a34dc40a907fc8
kmw2378 <rlarla677@gmail.com>
2025-02-09 23:20:08 +0900

    [FEAT] 인증 코드 전송, 이메일 수정 로직 변경 (#102) - 인증 코드 전송을 Spring Event Listener 을 사용하도록 변경 - 인증 코드 확인을 VerificationCodeService 를 사용하도록 변경

--- src/main/java/ject/componote/domain/auth/application/MemberService.java
+++ src/main/java/ject/componote/domain/auth/application/MemberService.java
@@ -6,6 +6,7 @@ import ject.componote.domain.auth.dto.find.response.MemberSummaryResponse;
 import ject.componote.domain.auth.dto.update.request.MemberEmailUpdateRequest;
 import ject.componote.domain.auth.dto.update.request.MemberNicknameUpdateRequest;
 import ject.componote.domain.auth.dto.update.request.MemberProfileImageUpdateRequest;
+import ject.componote.domain.auth.dto.verify.event.EmailVerificationCodeSendEvent;
 import ject.componote.domain.auth.dto.verify.request.MemberEmailVerificationRequest;
 import ject.componote.domain.auth.error.DuplicatedEmailException;
 import ject.componote.domain.auth.error.DuplicatedNicknameException;
@@ -15,9 +16,9 @@ import ject.componote.domain.auth.model.AuthPrincipal;
 import ject.componote.domain.auth.model.Email;
 import ject.componote.domain.auth.model.Nickname;
 import ject.componote.domain.auth.model.ProfileImage;
-import ject.componote.infra.mail.application.MailService;
 import ject.componote.infra.storage.application.StorageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,9 +26,10 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberService {
+    private final ApplicationEventPublisher eventPublisher;
     private final StorageService storageService;
-    private final MailService mailService;
     private final MemberRepository memberRepository;
+    private final VerificationCodeService verificationCodeService;
 
     public MemberSummaryResponse getMemberSummary(final AuthPrincipal authPrincipal) {
         final Long memberId = authPrincipal.id();
@@ -67,13 +69,11 @@ public class MemberService {
 
     @Transactional
     public void updateEmail(final AuthPrincipal authPrincipal, final MemberEmailUpdateRequest request) {
-        final Email email = Email.from(request.email());
-        validateDuplicatedEmail(email);
-
         final Member member = findMemberById(authPrincipal.id());
+        final Email email = Email.from(request.email());
         validateSameEmail(member, email);
-
-        mailService.verifyEmailCode(request.email(), request.verificationCode());
+        validateDuplicatedEmail(email);
+        verificationCodeService.verifyEmailCode(request.email(), request.verificationCode());
         member.updateEmail(email);
     }
 
@@ -84,7 +84,7 @@ public class MemberService {
         final Member member = findMemberById(authPrincipal.id());
         validateSameEmail(member, email);
 
-        mailService.sendVerificationCode(request.email());
+        eventPublisher.publishEvent(EmailVerificationCodeSendEvent.from(email));
     }
 
     private Member findMemberById(final Long memberId) {
