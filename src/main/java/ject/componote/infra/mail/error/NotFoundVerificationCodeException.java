8f6f3ba2f9bf691034c30a48b0b3fab8c0bb0f4d
kmw2378 <rlarla677@gmail.com>
2025-02-09 23:16:51 +0900

    [REFACTOR] MailException 삭제 (#102)

--- src/main/java/ject/componote/infra/mail/error/NotFoundVerificationCodeException.java
+++
@@ -1,9 +0,0 @@
-package ject.componote.infra.mail.error;
-
-import org.springframework.http.HttpStatus;
-
-public class NotFoundVerificationCodeException extends MailException {
-    public NotFoundVerificationCodeException() {
-        super("인증 코드가 만료되었거나 잘못되었습니다.", HttpStatus.NOT_FOUND);
-    }
-}
