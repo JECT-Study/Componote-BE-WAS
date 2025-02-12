0f18c1e9223a637b866156047d2eddd415b73ce4
kmw2378 <rlarla677@gmail.com>
2025-02-12 14:48:48 +0900

    [HOTFIX] 회원 간단 조회 API 응답 수정 - 직업 필드 추가

--- src/main/java/ject/componote/domain/auth/dao/MemberSummaryDao.java
+++ src/main/java/ject/componote/domain/auth/dao/MemberSummaryDao.java
@@ -1,8 +1,9 @@
 package ject.componote.domain.auth.dao;
 
+import ject.componote.domain.auth.domain.Job;
 import ject.componote.domain.auth.model.Email;
 import ject.componote.domain.auth.model.Nickname;
 import ject.componote.domain.auth.model.ProfileImage;
 
-public record MemberSummaryDao(Email email, Nickname nickname, ProfileImage profileImage) {
+public record MemberSummaryDao(Email email, Nickname nickname, ProfileImage profileImage, Job job) {
 }
--- src/main/java/ject/componote/domain/auth/dto/find/response/MemberSummaryResponse.java
+++ src/main/java/ject/componote/domain/auth/dto/find/response/MemberSummaryResponse.java
@@ -2,12 +2,13 @@ package ject.componote.domain.auth.dto.find.response;
 
 import ject.componote.domain.auth.dao.MemberSummaryDao;
 
-public record MemberSummaryResponse(boolean isEmailRegistered, String nickname, String profileImageUrl) {
+public record MemberSummaryResponse(boolean isEmailRegistered, String nickname, String profileImageUrl, String job) {
     public static MemberSummaryResponse from(final MemberSummaryDao memberSummaryDao) {
         return new MemberSummaryResponse(
                 memberSummaryDao.email() != null,   // 좀 애매한 것 같음
                 memberSummaryDao.nickname().getValue(),
-                memberSummaryDao.profileImage().toUrl()
+                memberSummaryDao.profileImage().toUrl(),
+                memberSummaryDao.job().name()
         );
     }
 }
